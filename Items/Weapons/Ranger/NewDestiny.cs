fda827bc577e76331fcfbc5ac9d8d7a50bba786b
Ethn <ethn.roth@gmail.com>
2020-12-29 00:12:57 -0700

    fiux

--- Items/Weapons/Ranger/NewDestiny.cs
+++ Items/Weapons/Ranger/NewDestiny.cs
@@ -1,3 +1,5 @@
+using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -10,33 +12,33 @@ namespace Runeterraria.Items.Weapons.Ranger
 		}
 
 		public override void SetDefaults() {
-			item.damage = 42; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
+			item.damage = 62; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
 			item.ranged = true; // sets the damage type to ranged
 			item.width = 40; // hitbox width of the item
 			item.height = 20; // hitbox height of the item
-			item.useTime = 75; // The item's use time in ticks (60 ticks == 1 second.)
-			item.useAnimation = 75; // The length of the item's use animation in ticks (60 ticks == 1 second.)
+			item.useTime = 65; // The item's use time in ticks (60 ticks == 1 second.)
+			item.useAnimation = 65; // The length of the item's use animation in ticks (60 ticks == 1 second.)
 			item.useStyle = ItemUseStyleID.HoldingOut; // how you use the item (swinging, holding out, etc)
 			item.noMelee = true; //so the item's animation doesn't do damage
-			item.knockBack = 8; // Sets the item's knockback. Note that projectiles shot by this weapon will use its and the used ammunition's knockback added together.
+			item.knockBack = 14; // Sets the item's knockback. Note that projectiles shot by this weapon will use its and the used ammunition's knockback added together.
 			item.value = 700000; // how much the item sells for (measured in copper)
 			item.rare = 3; // the color that the item's name will be in-game
 			item.UseSound = SoundID.Item11; // The sound that this item plays when used.
 			item.autoReuse = false; // if you can hold click to automatically use it again
 			item.shoot = 10; //idk why but all the guns in the vanilla source have this
-			item.shootSpeed = 16f; // the speed of the projectile (measured in pixels per frame)
+			item.shootSpeed = 13f; // the speed of the projectile (measured in pixels per frame)
 			item.useAmmo = AmmoID.Bullet; // The "ammo Id" of the ammo item that this weapon uses. Note that this is not an item Id, but just a magic value.
 		}
-}
     public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
     {
       int numberProjectiles = 5; // 4 or 5 shots
       for (int i = 0; i < numberProjectiles; i++)
       {
-        Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(30)); // 30 degree spread.
+        Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(8)); // 30 degree spread.
 
         Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
       }
       return false;
     }
   }
+}
