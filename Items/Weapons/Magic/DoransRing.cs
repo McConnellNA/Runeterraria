d0dc80a0a372d7116a7067aebc3918058fc8a89e
Ethn <ethn.roth@gmail.com>
2020-12-28 22:30:09 -0700

    fix

--- Items/Weapons/Magic/DoransRing.cs
+++ Items/Weapons/Magic/DoransRing.cs
@@ -9,23 +9,23 @@ namespace ExampleMod.Items.Weapons
 	public class ExampleStaff : ModItem
 	{
 		public override void SetStaticDefaults() {
-			Tooltip.SetDefault("This is a modded magic weapon.");
+			Tooltip.SetDefault("Crafted by the once great artificer, Doran");
 			Item.staff[item.type] = true; //this makes the useStyle animate as a staff instead of as a gun
 		}
 
 		public override void SetDefaults() {
-			item.damage = 20;
+			item.damage = 15;
 			item.magic = true;
-			item.mana = 12;
-			item.width = 40;
-			item.height = 40;
-			item.useTime = 25;
-			item.useAnimation = 25;
+			item.mana = 5;
+			item.width = 5;
+			item.height = 5;
+			item.useTime = 30;
+			item.useAnimation = 30;
 			item.useStyle = ItemUseStyleID.HoldingOut;
 			item.noMelee = true; //so the item's animation doesn't do damage
-			item.knockBack = 5;
-			item.value = 10000;
-			item.rare = ItemRarityID.Green;
+			item.knockBack = 2.8;
+			item.value = Item.buyPrice(400);
+			item.rare = 1;
 			item.UseSound = SoundID.Item20;
 			item.autoReuse = true;
 			item.shoot = ModContent.ProjectileType<SparklingBall>();
