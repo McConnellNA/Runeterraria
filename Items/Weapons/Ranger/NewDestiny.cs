438309f4f956a7e9c13c6831d4ae3b562a0edb3a
Locus <45237200+MetaLocus@users.noreply.github.com>
2020-12-29 00:37:07 -0700

    Sounds

--- Items/Weapons/Ranger/NewDestiny.cs
+++ Items/Weapons/Ranger/NewDestiny.cs
@@ -8,7 +8,7 @@ namespace Runeterraria.Items.Weapons.Ranger
 	public class NewDestiny : ModItem
 	{
 		public override void SetStaticDefaults() {
-			Tooltip.SetDefault("This is a modded gun.");
+			Tooltip.SetDefault("From an old past.");
 		}
 
 		public override void SetDefaults() {
@@ -23,7 +23,7 @@ namespace Runeterraria.Items.Weapons.Ranger
 			item.knockBack = 10; // Sets the item's knockback. Note that projectiles shot by this weapon will use its and the used ammunition's knockback added together.
 			item.value = 700000; // how much the item sells for (measured in copper)
 			item.rare = 3; // the color that the item's name will be in-game
-			item.UseSound = SoundID.Item11; // The sound that this item plays when used.
+			item.UseSound = SoundID.Item36; // The sound that this item plays when used.
 			item.autoReuse = false; // if you can hold click to automatically use it again
 			item.shoot = 10; //idk why but all the guns in the vanilla source have this
 			item.shootSpeed = 7f; // the speed of the projectile (measured in pixels per frame)
